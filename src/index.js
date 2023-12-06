a6fc91c442f944accac164a4ed05e763dd299e18
Rahaf ALsaeed <rahafalsaeed00@gmail.com>
2023-12-06 21:07:24 +0300

    added get single product

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ const express = require('express');
 const app = express();
 
 const port = 3005;
+const products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: 123
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: 456
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: 789
+    }
+]
 
 app.get('/', (req, res) => {
   res.send('Welcome to express-server');
@@ -10,24 +27,24 @@ app.get('/', (req, res) => {
 
 app.get('/products', (req, res) => {
     res.send({
-        products: [
-            {
-                id: 1,
-                name: 'Product 1',
-                price: 123
-            },
-            {
-                id: 2,
-                name: 'Product 2',
-                price: 456
-            },
-            {
-                id: 3,
-                name: 'Product 3',
-                price: 789
-            }
-        ]
+        products : products
     });
+})
+
+app.get('/products/:id', (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const product = products.find((product) => product.id === id);
+        if (!product) {
+            res.status(404).send(`product not found with id: ${id}`)
+        }
+        res.status(200).send({
+            product : product
+        });
+    } catch (error) {
+        res.status(500).send('error')
+    }
+    
 })
 
 app.listen(port, () => {
