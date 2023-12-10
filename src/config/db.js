5310baf93de8f5ca35979eacff83eac1a6d2b0d7
Rahaf ALsaeed <rahafalsaeed00@gmail.com>
2023-12-11 01:00:21 +0300

    adding mongo url

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const { dev } = require(".");
 exports.connectDB = async () => {
   try {
     await mongoose.connect(
-      dev.db.MONGODB_URI
+      dev.db.MONGODB_URI || 'mongodb+srv://RahafAlsaeed:Rahaf12345@cluster0.dhetf0m.mongodb.net/test?retryWrites=true&w=majority'
     );
     console.log("database is connected");
   } catch (error) {
