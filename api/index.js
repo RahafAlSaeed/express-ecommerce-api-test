dc52f93298116d77ef9709f2f454f761657e3625
Rahaf ALsaeed <rahafalsaeed00@gmail.com>
2023-12-11 03:43:29 +0300

    first commit

--- api/index.js
+++ api/index.js
@@ -3,22 +3,24 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const { connectDB } = require("../config/db");
-
 const productRoute = require("../routers/productsRouter");
 
 const app = express();
 
 const port = process.env.PORT;
 
+connectDB();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to express-server");
 });
 
+app.use("/products", productRoute);
+
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
-  await connectDB();
 });
